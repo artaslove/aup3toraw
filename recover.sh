b4a574dde629fed93bba96c24b3b592ea93843cc
Tony <tonyscomputerconsulting@gmail.com>
2022-04-08 20:17:36 -0700

    Add files via upload

--- recover.sh
+++ recover.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-# rebuild db also
-sqlite3 "$1" ".dump" > "$1.sql"
-sed -i '/BEGIN TRANSACTION/d;/ROLLBACK/d' "$1.sql" 
-sqlite3 "$1_rebuilt.aup3" < "$1.sql"
+# rebuild sqlite3 db
+#
+# Usage: recover.sh [corrupt sqlite3 filename] [recovered sqlite3 filename]
+
+ver=`sqlite3 "$1" "select sqlite_version();"`
+if [[ "3.22.0" -gt "$ver" ]]; then
+ sqlite3 "$1" ".dump" | sqlite3 "$2"
+else
+ sqlite3 "$1" ".recover" | sqlite3 "$2"
+fi
+
+# sed -i '/BEGIN TRANSACTION/d;/ROLLBACK/d' "$1.sql" 
+# sqlite3 "$1_rebuilt.aup3" < "$1.sql"
 # aup3toraw -i "$1_rebuilt.aup3" -o "$1_rebuilt.raw"
