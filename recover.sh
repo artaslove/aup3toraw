0aa7be34a9022c73e3d4f6300f1c50dbb1f7b4cc
Tony <tonyscomputerconsulting@gmail.com>
2022-04-08 20:20:31 -0700

    Add files via upload

--- recover.sh
+++ recover.sh
@@ -10,6 +10,8 @@ else
  sqlite3 "$1" ".recover" | sqlite3 "$2"
 fi
 
+
+# sqlite3 "$1" ".dump" > "$1.sql"
 # sed -i '/BEGIN TRANSACTION/d;/ROLLBACK/d' "$1.sql" 
 # sqlite3 "$1_rebuilt.aup3" < "$1.sql"
 # aup3toraw -i "$1_rebuilt.aup3" -o "$1_rebuilt.raw"
